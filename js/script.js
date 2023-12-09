9c52e3fdc904c748ca11c9a6be0fcfb4aae24bad
Egor <mregor@bk.ru>
2023-12-09 11:24:17 +0200

    mom

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let incr = 10,
+    decr =10;
+
+incr++;
+decr--;
+console.log(incr++);
+console.log(incr);
+console.log(5%2);    
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked    && isClosegi);
